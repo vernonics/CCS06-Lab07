77abf20ef0ba5d040d59585798dec870dff6c78a
Veronica Jane M. Tibe <tibe.veronicajane@auf.edu.ph>
2023-04-26 15:11:10 +0800

    My website initial files

---
+++ download.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+require "vendor/autoload.php";
+use App\QuestionManager;
+
+$manager = new QuestionManager;
+if($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST['download']))
+{
+    $manager->kekw($_SESSION['answers']);
+}
+
